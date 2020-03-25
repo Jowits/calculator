ce4f103d6d3043a15826044896ec49b2525e60da
Jowits <jowita.lapies@gmail.com>
2020-03-25 16:25:42 +0000

    caculator set up

---
+++ calculator.py
@@ -0,0 +1,4 @@
+class Calculator:
+    num1 = int(input("What is your first number you would like me to calculate?(not a decimal)"))
+    num2 = int(input("What is your second number you would like me to calculate?(not a decimal)"))
+    calculation = input("How would you like me to calculate this?")
