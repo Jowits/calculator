d87dc9bf29d9a0c39001528c6546e9bcafb0d667
Jowits <jowita.lapies@gmail.com>
2020-03-25 16:27:21 +0000

    addition set up

--- calculator.py
+++ calculator.py
@@ -2,3 +2,14 @@ class Calculator:
     num1 = int(input("What is your first number you would like me to calculate?(not a decimal)"))
     num2 = int(input("What is your second number you would like me to calculate?(not a decimal)"))
     calculation = input("How would you like me to calculate this?")
+
+    def add():
+        if calculation in ["+", "add", "addition"]
+        answer = num1 + num2
+        print(answer)
+
+    
+
+
+
+    add()
